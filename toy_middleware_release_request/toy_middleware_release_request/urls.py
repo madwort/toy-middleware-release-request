8b72f09e301007cfcf81845ee88c181de06b36a7
Tom Ward <tomward@fmail.co.uk>
2025-03-13 17:43:37 +0000

    Initial check-in of basic app

---
+++ toy_middleware_release_request/toy_middleware_release_request/urls.py
@@ -0,0 +1,26 @@
+"""
+URL configuration for toy_middleware_release_request project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+import toy_middleware_release_request.views.request
+
+urlpatterns = [
+    path('request/view/<str:release_request_id>', toy_middleware_release_request.views.request_view),
+    path('request/edit/<str:release_request_id>', toy_middleware_release_request.views.request_edit),
+    path('request/remove/<str:release_request_id>', toy_middleware_release_request.views.request_remove),
+    path('admin/', admin.site.urls),
+]
